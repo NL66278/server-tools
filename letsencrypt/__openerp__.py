e1a1378dc83bb25381b9f3f7bba1da1d334579e1
Roberto Fichera <robyf@tekno-soft.it>
2017-05-19 18:45:56 +0200

    [10.0][MIG] Migrate letsencrypt

--- letsencrypt/__openerp__.py
+++ letsencrypt/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2016 Therp BV <https://therp.nl>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Let's encrypt",
     "version": "8.0.1.0.0",
