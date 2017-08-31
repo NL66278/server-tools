cbd4fe38d45c2dbdee49c33334dbaaec5a5e1a1a
lfreeke <lfreeke@therp.nl>
2017-08-31 14:11:29 +0200

    [MIG] base_view_inheritance_extension to 10.0

--- base_view_inheritance_extension/tests/test_base_view_inheritance_extension.py
+++ base_view_inheritance_extension/tests/test_base_view_inheritance_extension.py
@@ -2,7 +2,7 @@
 # © 2016 Therp BV <http://therp.nl>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from lxml import etree
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestBaseViewInheritanceExtension(TransactionCase):
